1f1134d382dbdc9487f3d4b4c9ea2a0427951429
unknown <stellazen.xd@gmail.com>
2024-07-06 15:40:17 -0300

    feat: componente TechSelector

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,74 @@
 <script lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { defineComponent, ref, computed } from 'vue'
 import ImageHeader from './components/ImageHeader.vue'
 import CardProject from './components/CardProject.vue'
+import TechSelector from './components/TechSelector.vue'
 import Data from './data/data.json'
-import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components:{
-  
+  components: {
     ImageHeader,
-    CardProject
+    CardProject,
+    TechSelector,
   },
-  data(){
-    return{
-      protjects: Data
+  setup() {
+    const selectedTechnology = ref<string | null>(null);
+    const projects = ref(Data);
+
+    const availableTechnologies = computed(() => {
+      const technologiesSet = new Set<string>();
+      projects.value.forEach(project => {
+        project.technologies.forEach((tech: string) => {
+          technologiesSet.add(tech);
+        });
+      });
+      return Array.from(technologiesSet);
+    });
+
+    const filteredProjects = computed(() => {
+      if (!selectedTechnology.value) {
+        return projects.value;
+      }
+      return projects.value.filter(project =>
+        project.technologies.includes(selectedTechnology.value)
+      );
+    });
+
+    const selectTechnology = (tech: string | null) => {
+      selectedTechnology.value = tech;
+    };
+
+    return {
+      selectedTechnology,
+      filteredProjects,
+      availableTechnologies,
+      selectTechnology,
     };
   }
 });
-
 </script>
 
 <template>
-  <ImageHeader /> 
+  <ImageHeader />
+  <div>
+    <TechSelector
+      tech="All"
+      :selectedTechnology="selectedTechnology"
+      @select="selectTechnology(null)"
+    />
+    <TechSelector
+      v-for="tech in availableTechnologies"
+      :key="tech"
+      :tech="tech"
+      :selectedTechnology="selectedTechnology"
+      @select="selectTechnology"
+    />
+  </div>
   <div>
     <CardProject
-      v-for="(project, index) in protjects"
-      :key= "index"
+      v-for="(project, index) in filteredProjects"
+      :key="index"
       :name="project.name"
       :description="project.description"
       :technologies="project.technologies"
@@ -34,9 +76,9 @@ export default defineComponent({
       :img="project.img"
     />
   </div>
-
 </template>
 
 <style scoped>
-
+/* Adicione estilos conforme necessário */
 </style>
+
