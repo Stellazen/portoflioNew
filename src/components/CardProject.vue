2e79253d62dc781fa02babb9028d48621aecaee6
unknown <stellazen.xd@gmail.com>
2024-07-06 12:21:37 -0300

    feat: imagens na tela

--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -7,25 +7,36 @@ const props = defineProps<{
   url: string,
   img: string,
 }>()
+
 </script>
 
 <template>
   <div class="container">
     <div class="description">
-        <h2>{{ name }}</h2>
-        <p>{{ description }}</p>
-        <div>{{ technologies }}</div>
+      <h2>{{ name }}</h2>
+      <p class="descritpion">{{ props.description }}</p>
+      <div>
+        <span v-for="(tech, index) in props.technologies" :key="index">{{ tech }}</span>
+      </div>
     </div>
-    <a :href="props.url">
-      <div class="project_photo" :style="{ backgroundImage: `url(${img})`}"></div>
-    </a>
+    <div class="project_photo" :style="{ backgroundImage: `url(${props.img})` }">
+      <a :href="props.url" target="_blank">clica</a>
+    </div>
   </div>
 </template>
 
 <style scoped>
+    .container{
+      display: flex;
+      margin: 15px;
+      justify-content: center;
+    }
+    .descritpion{
+      max-width: 500px
+    }
     .project_photo{
-        width: 200px;
-        height: 200px;
-        
+        width: 500px;
+        height: 300px;
+        background: no-repeat center center cover;
     }
 </style>
