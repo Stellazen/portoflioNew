888f9cd98814fe25158468f5b73a1437c926ef86
unknown <stellazen.xd@gmail.com>
2024-07-08 11:24:57 -0300

    Feat: criação componente about + estilização

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { onMounted } from 'vue';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+onMounted(() => {
+  AOS.init({
+    // Aqui você pode adicionar configurações globais, se necessário
+    duration: 1000,
+    once: true,
+  });
+});
+
+</script>
+
+<template>
+    <div class="about">
+        <img src="../assets/stella.png" class="about__image" alt="" data-aos="fade-up">
+        <div class="about__description" data-aos="fade-up">
+            <p>texto sobre mim texto sobre mim texto sobre mim texto sobre mim texto sobre mim texto sobre mim texto sobre mim texto sobre mim Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam doloribus, assumenda consequuntur laudantium deleniti debitis sed aliquam odio, temporibus fugiat aliquid illum magnam iste corrupti incidunt minima similique praesentium? Molestiae.</p>
+        </div>
+    </div>
+</template>
+
+<style setup>
+    .about{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 50px;
+        height: 50vh;
+    }
+    .about__image{
+        width: 350px;
+        border-radius: 50%;
+    }
+    .about__description{
+        max-width: 600px;
+        padding-left: 50px;
+        color: var(--white)
+    }
+</style>
