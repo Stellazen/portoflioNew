e0874e9e546bc93ca24f3fc51347ac5555a5b208
unknown <stellazen.xd@gmail.com>
2024-07-08 21:16:18 -0300

    style: estilização cards e header

--- src/components/CardProject.vue
+++ src/components/CardProject.vue
@@ -30,25 +30,29 @@ const props = defineProps<{
       <div class="container__description__technologies" data-aos="fade-right">
         <span class="container__description__technology" v-for="(tech, index) in props.technologies" :key="index">{{ tech }}</span>
       </div>
-    </div>
-    <a class="container__link" data-aos="fade-left" :href="props.url" target="_blank">
+      <a :href="props.url" target="_blank">
+        <div class="container__link" data-aos="fade-right">
+          <p>Link do Projeto</p>
+        </div>
+      </a>
+      </div>
     <div class="container__photo" data-aos="fade-left" :style="{ backgroundImage: `url(${props.img})` }">
-    </div></a>
+    </div>
   </div>
 </template>
 
 <style scoped>
     .container{
       display: flex;
-      margin: 20px;
+      margin: 100px 0 100px 0;
       gap: 20px;
       border-radius: 15px;
       justify-content: center;
     }
     .container__title{
       margin: 5px 0 5px 0;
-      background-color: var(--red);
       font-family: var(--font-title);
+      text-decoration: underline var(--red);
     }
     .container__description{
       max-width: 500px;
@@ -65,11 +69,31 @@ const props = defineProps<{
       border-radius: 5px;
       color: lightgray;
     }
+    .container__link{
+      background-color: var(--black);
+      padding: 5px;
+      border-radius: 5px;
+      margin-top: 20px;
+      text-align: center;
+      max-width: 200px;
+      transition:0.5ms
+    }
+    .container__link:hover{
+      background-color: var(--soft_green);
+    }
+    a{
+      text-decoration: none;
+      color: var(--white);
+    }
+    a:hover{
+      color: black;  
+    }
     .container__photo{
         width: 500px;
         height: 300px;
         background: no-repeat center center cover;
         align-self: center;
+        border-radius: 10px;
     }
     .container__photo:hover{
       opacity: 0.3;
